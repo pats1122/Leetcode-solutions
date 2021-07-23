b88eac485f48a24251cac282014847921e50b8ef
pats1122 <35428632+pats1122@users.noreply.github.com>
2021-07-23 11:22:43 +0530

    Time: 348 ms (16.78%), Space: 19.5 MB (46.05%) - LeetHub

--- palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
+++ palindrome-partitioning-ii/palindrome-partitioning-ii.cpp
@@ -1,26 +1,31 @@
 class Solution {
 public:
-    bool isPalindrome(string &s, int start, int end){
-        while(start < end){
-            if(s[start]!=s[end])return false;
-            start++;
-            end--;
+    //s was not passed as reference so it gives error for large s
+    bool isPalindrome(string &s){
+        int lo=0, hi=s.size()-1;
+        while(lo<hi){
+            if(s[lo]!=s[hi]) return false;
+            lo++;
+            hi--;
         }
         return true;
     }
     int minCut(string s) {
         int n = s.size();
-        vector<int> dp(n,INT_MAX);
-        dp[n-1] = 0;
+        vector<int> dp(n, INT_MAX);
         
-        for(int i=n-2; i>=0; i--){
+        dp[n-1] = 0;
+        for(int i=n-1; i>=0; i--){
+            string curr = "";
             for(int j=i; j<n; j++){
-                if(!isPalindrome(s,i,j))continue;
+                curr += s[j];
                 
-                if(j<n-1)
-                     dp[i] = min(dp[i], 1+dp[j+1]);
-                else
-                    dp[i] =0;
+                if(isPalindrome(curr)){
+                    if(j+1 != n)
+                        dp[i] = min(dp[i], 1+dp[j+1]);
+                    else
+                        dp[i] = 0;
+                }
             }
         }
         return dp[0];
