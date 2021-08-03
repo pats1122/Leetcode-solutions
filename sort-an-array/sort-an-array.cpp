09a47bac01b30f8aabda4283b9b0d3fcae6c44f3
pats1122 <35428632+pats1122@users.noreply.github.com>
2021-08-03 21:20:55 +0530

    Time: 40 ms (96.72%), Space: 29.3 MB (35.46%) - LeetHub

--- sort-an-array/sort-an-array.cpp
+++ sort-an-array/sort-an-array.cpp
@@ -1,50 +1,30 @@
-//merge sort
 class Solution {
 public:
-    void merge(vector<int> &nums, int start, int end, int mid ){
-        int n1 = mid-start+1;
-        int n2 = end-mid;
-        vector<int> num1 (n1);
-        vector<int> num2 (n2);
-        
-        for(int i=0; i<n1; i++)
-            num1[i] = nums[start+i];
-        for(int i=0; i<n2; i++)
-            num2[i] = nums[i+mid+1];
-        
-        int p1=0,p2=0,p=start;
-        while(p1<n1 && p2<n2){
-            if(num1[p1]<=num2[p2]){
-                nums[p] = num1[p1];
-                p++;
-                p1++;
-            }
-            else{
-                nums[p] = num2[p2];
-                p++;
-                p2++;
-            }
-        }
-        while(p1<n1){
-            nums[p] = num1[p1];
-            p++; p1++;
-        }
-        while(p2<n2){
-            nums[p] = num2[p2];
-            p++; p2++;
-        }
-    }
-    void mergeSort(vector<int> &nums, int start, int end){
-        if(start<end){
-            int mid = start+(end-start)/2;
-            mergeSort(nums,start,mid);
-            mergeSort(nums,mid+1,end);
-            merge(nums,start,end, mid);
-        }
-    }
     vector<int> sortArray(vector<int>& nums) {
-        //merge sort
-        mergeSort(nums,0, nums.size()-1);
-        return nums;
+        //counting sort
+        //find max and min element 
+        int min = *min_element(nums.begin(), nums.end());
+        int max = *max_element(nums.begin(), nums.end());
+        
+        //declare freq array to keep frequency of each element
+        vector<int> freq(max-min+1, 0);
+        
+        //store frequency of each element in range
+        for(int i=0; i<nums.size(); i++){
+            freq[nums[i]-min]++;
+        }
+        
+        //calculate prefix sum
+        for(int i=1; i<freq.size(); i++)
+            freq[i] += freq[i-1];
+        
+        //start putting elements at right pos from end
+        vector<int> ans(nums.size());
+        for(int i=nums.size()-1; i>=0; i--){
+            int idx = freq[nums[i]-min] - 1 ;
+            ans[idx] = nums[i];
+            freq[nums[i]-min]--;
+        }
+        return ans;
     }
 };
