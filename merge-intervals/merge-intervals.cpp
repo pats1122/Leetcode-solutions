bcf76511d9aec076213fd889c4680551b79c9d0d
pats1122 <35428632+pats1122@users.noreply.github.com>
2021-08-03 13:07:32 +0530

    Time: 20 ms (64.24%), Space: 14.1 MB (94.97%) - LeetHub

--- merge-intervals/merge-intervals.cpp
+++ merge-intervals/merge-intervals.cpp
@@ -1,25 +1,23 @@
-bool compare(vector<int> &a, vector<int> &b){
-        if(a[0]==b[0]) return a[1]<b[1];
-        return a[0]<b[0];
-    }
 class Solution {
 public:
-    
     vector<vector<int>> merge(vector<vector<int>>& intervals) {
-         vector<int> temp;
         vector<vector<int>> res;
-         sort(intervals.begin(), intervals.end(), compare);
-         temp = intervals[0];
+        sort(intervals.begin(), intervals.end());
         
-        for(int i=1; i<intervals.size(); i++){
-            if(temp[1] >= intervals[i][0])
-                temp[1] = max(temp[1], intervals[i][1]);
+        int start=intervals[0][0];
+        int end = intervals[0][1];
+        
+        for(int i=1;i<intervals.size(); i++){
+            if(intervals[i][0]>end){
+                res.push_back({start,end});
+                start = intervals[i][0];
+                end = intervals[i][1];
+            }
             else{
-                res.push_back(temp);
-                temp = intervals[i];
+                end = max(end, intervals[i][1]);
             }
         }
-        res.push_back(temp);
+        res.push_back({start,end});
         return res;
     }
 };
