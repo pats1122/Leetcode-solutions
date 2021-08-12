03ef3a1fca7617905e1541b0ed4e289b20643ee3
pats1122 <35428632+pats1122@users.noreply.github.com>
2021-08-12 09:52:09 +0530

    Time: 0 ms (100.00%), Space: 9.3 MB (8.80%) - LeetHub

--- restore-ip-addresses/restore-ip-addresses.cpp
+++ restore-ip-addresses/restore-ip-addresses.cpp
@@ -1,40 +1,40 @@
 class Solution {
 public:
-    void restore(string &s, int start,vector<string> curr, vector<string> &res){
+    void restore(string &s, int start, vector<string> curr, vector<string> &res){
+        int n = s.size();
         if(start==s.size() && curr.size()==4){
-            string ip = "";
-            for(auto s:curr) ip+= s;
-            ip.pop_back(); //to remove last "."
+            string ip="";
+            for(auto c: curr) ip+= c;
+            ip.pop_back();
             res.push_back(ip);
             return;
         }
-        
-        if(start>=s.size() || curr.size()>=4)return;
-        
+        if(start>=n || curr.size()>=4) return;
         
         //1 digit
-        curr.push_back(s.substr(start,1) + ".");
-        restore(s,start+1,curr, res);
-        curr.pop_back();
-
+            curr.push_back(s.substr(start,1) + ".");
+            restore(s,start+1,curr,res);
+             curr.pop_back();
         
-        //2 digit
+            
+        //2 digits
         if(stoi(s.substr(start,2))>=10 ){
             curr.push_back(s.substr(start,2) + ".");
-            restore(s,start+2,curr, res);
+            restore(s,start+2,curr,res);
             curr.pop_back();
         }
         
-        //3 digit
-        if(stoi(s.substr(start,3))>=100 && stoi(s.substr(start,3))<256){
+        //3 digits
+        if(stoi(s.substr(start,3))>=100 && stoi(s.substr(start,3))<=255){
             curr.push_back(s.substr(start,3) + ".");
-            restore(s,start+3,curr, res);
+            restore(s,start+3,curr,res);
             curr.pop_back();
         }
+        
     }
     vector<string> restoreIpAddresses(string s) {
-        if(s.size()>12) return {};
-        vector<string> res,curr;
+        vector<string> curr;
+        vector<string> res;
         restore(s,0,curr,res);
         return res;
     }
